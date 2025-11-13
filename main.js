f850865690da5e748c6a788d8361c56661f709b2
muhawwal-git <muhawwal9k@gmail.com>
2025-11-14 05:51:09 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+alert("hai");
+const button = document.querySelector ("button")
+    button.addEventListener ("click", function(){
+        alert ("Selamat Datang");
+        console.log ("Berhasil Ditekan");
+    });
